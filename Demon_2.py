658a0367be3ebaa59064f5b0c1760e94c93f5db8
sharukkhan022 <sharukkhanpathan226@gmail.com>
2026-02-15 09:47:33 +0530

    Initial commit

---
+++ Demon_2.py
@@ -0,0 +1,7 @@
+cd Demon_slayer_repo
+git init
+git remote add origin https://github.com/Sharukkhan022/REPO_NAME.git
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git push -u origin main
